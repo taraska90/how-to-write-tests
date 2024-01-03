1094a3ae3c8392b4673880139b28dbc54b74cb29
tar <taras061090@gmail.com>
2024-01-03 09:55:11 +0700

    implement tests and developing hash function

--- hashtable.py
+++ hashtable.py
@@ -1,11 +1,17 @@
+BLANK = object()
+
 class HashTable():
     def __init__(self, capacity):
-       self.values = capacity * [None]
+       self.values = capacity * [BLANK]
 
     def __len__(self):
         return len(self.values)
 
     def __setitem__(self, key, value):
-        self.values.append(value)
+        index = hash(key) % len(self)
+        self.values[index] = value
 
+    def __getitem__(self, key):
+        index = hash(key) % len(self)
+        return self.values[index]
 
--- test_hashtable.py
+++ test_hashtable.py
@@ -1,5 +1,14 @@
 import pytest
-from hashtable import HashTable
+from hashtable import HashTable, BLANK
+
+@pytest.fixture
+def hash_table():
+    sample_data = HashTable(capacity=100)
+    sample_data["hola"] = "hello"
+    sample_data[98.6] = 37
+    sample_data[False] = True
+    return sample_data
+
 
 def test_should_create_hashtable():
     assert HashTable(capacity=100) is not None
@@ -9,7 +18,7 @@ def test_should_report_capacity():
 
 def test_should_create_empty_value_slots():
     # Given
-    expected_values = [None, None, None]
+    expected_values = [BLANK, BLANK, BLANK]
     hashtable = HashTable(capacity=3)
     # When
     actual_values = hashtable.values
@@ -29,6 +38,25 @@ def test_should_insert_key_value_pairs():
     assert "hello" in hash_table.values
     assert 37 in hash_table.values
     assert True in hash_table.values
+    assert len(hash_table) == 100
+
+def test_should_not_contain_none_value_when_created():
+    assert None not in HashTable(capacity=100).values
+
+
+def test_should_insert_none_values():
+    #given
+    hash_table = HashTable(capacity=100)
+    #when
+    hash_table['key'] = None
+    #then
+    assert None in hash_table.values
+
+def test_should_find_value_by_key(hash_table):
+    assert hash_table["hola"] == "hello"
+    assert hash_table[98.6] == 37
+    assert hash_table[False] is True
+
 
 @pytest.mark.skip
 def test_should_not_shrink_when_removing_elemnts():
